c77eafd7f98000cf9c741ecaa3c4ea9b2527fa72
and05mak12 <117042079+and05mak12@users.noreply.github.com>
2022-10-30 23:33:21 +0300

    Add files via upload

---
+++ App/scripts/js-script.js
@@ -0,0 +1,12 @@
+function superClick(el) {
+	el.style.background ='orange';
+	el.style.color = 'green';
+}
+
+var h3Showed = false;
+
+function changeDisplay() {
+	const header3 = document.getElementById("1")
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
